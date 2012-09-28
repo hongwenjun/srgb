82f2065d9cd4e22ac18e8c5a84efb867577177a8
hongwenjun <hongwenjun@gmail.com>
2012-09-28 02:53:01 +0000

    auto it = sList.begin();     pyDictBin.write((char*) & (*it) , sizeof(pinyinUnit) * sList.size());  // 一次性把容器写文件

--- stl11x/Lambda.cpp
+++ stl11x/Lambda.cpp
@@ -7,14 +7,14 @@
 #include <string.h>
 using namespace std;
 
-struct pyUnit {     // 拼音单元数据结构
-    string word ;
-    string py ;
+struct pinyinUnit {     // 拼音单元数据结构
+    char word[8] ;
+    char py[32] ;
     int Pert;  // 单词 拼音 百分比
 };
-typedef vector<pyUnit> StringList;
+typedef vector<pinyinUnit> StringList;
 
-bool cmp(const pyUnit a,  const pyUnit b)   //  比较函数cmp
+bool cmp(const pinyinUnit a,  const pinyinUnit b)   //  比较函数cmp
 {   return a.Pert < b.Pert;   }
 
 void loadPYdict(fstream& file, StringList& resultList);  // 加载拼音字典file 到容器resultList
@@ -34,11 +34,23 @@ int main()
 
     //使用C++11 Lambda 函数，按拼音排序
     sort(sList.begin(), sList.end() ,
-    [](const pyUnit a,  const pyUnit b) { return a.py < b.py; });
+    [](const pinyinUnit a,  const pinyinUnit b) { return strcmp(a.py , b.py) < 0; });
 
     //使用C++11 Lambda 函数，遍历输出
     for_each(sList.begin(), sList.end(),
-    [](const pyUnit i) { cout << i.word << i.py << i.Pert << endl ;});
+    [](const pinyinUnit i) { cout << i.word << i.py << i.Pert << endl ;});
+
+
+    fstream pyDictBin("pybin.dat" , ios_base::out | ios_base::binary);
+
+//    for (auto i = 0; i != sList.size(); ++i)                // 遍历写文件
+//        pyDictBin.write((char*) &sList[i] , sizeof(pinyinUnit));
+//
+//    for (auto it = sList.begin(); it != sList.end(); ++it)  // 遍历写文件
+//        pyDictBin.write((char*) & (*it) , sizeof(pinyinUnit));   // 迭代器要先解引用，然后再取地址
+
+    auto it = sList.begin();
+    pyDictBin.write((char*) & (*it) , sizeof(pinyinUnit) * sList.size());  // 一次性把容器写文件
 
     return 0;
 }
@@ -46,11 +58,12 @@ int main()
 // 加载拼音字典file 到容器resultList
 void loadPYdict(fstream& file, StringList& resultList)
 {
-    char word[64] ;
-    char py[64] ;
+    char word[64] = {0} ;
+    char py[64] = {0} ;
     int Pert;  // 单词 拼音 百分比
     string line;
-    pyUnit tmp;
+    pinyinUnit tmp ;
+    memset(&tmp, 0 , sizeof(tmp));
     stringstream oss;    // 定义string流
     oss << file.rdbuf();
 
@@ -59,8 +72,8 @@ void loadPYdict(fstream& file, StringList& resultList)
             continue;
 
         sscanf(line.c_str(), "%s %s %d" , word, py , &Pert);    // 格式化读取一行
-        tmp.word = word;
-        tmp.py = py;
+        strcpy(tmp.word , word);
+        strcpy(tmp.py , py);
         tmp.Pert = Pert;
         resultList.push_back(tmp);  // 插入容器
     }
@@ -68,9 +81,11 @@ void loadPYdict(fstream& file, StringList& resultList)
 
 /*拼音字典文件： py.dat
 中文  Zhōngwén  99
-中国  Zhōngguó  1
+中国   Zhōngguó  1
 浙江  Zhèjiāng   5
-北京  Běijīng  9
-上海  Shànghǎi   8
+北京 Běijīng  9
+上海 Shànghǎi   8
+印刷 Yinshua  8
+虫虫  Chongchong 4
 */
 
