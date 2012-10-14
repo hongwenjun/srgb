1f05348334b7ddfd7d89104d8f1d2115eca20001
hongwenjun <hongwenjun@gmail.com>
2012-10-14 16:32:17 +0000

    测试 catch (exception& e)

--- cncpp/throw.cpp
+++ cncpp/throw.cpp
@@ -1,24 +1,45 @@
 #include <iostream>
+#include <stdio.h>
 using namespace std;
 
 void test(bool bl)
 {
     if (bl)
-        throw "你好世界!";    
+        throw "你好世界!";
     else
         throw "Hello world!";  // 抛出异常信息，C风格字符串
 }
 
 int main(int argc, char* argv[])
 {
-    try {                   // 异常测试
-        test(false);
-    } catch (const char* s) {  // 只能只读使用异常信息
-        cout << s << endl;
-        return -88;     // 直接结束程序
+    try {                       // 异常测试
+
+        //  test(false);        // 跳到catch (...)
+        while (1)
+            int* myarray = new int[1024*1024];
+
+
+    } catch (exception& e) {
+        cerr << "EXCEPCTION: " << e.what() << endl;
+        return -1;
+    } catch (...) {
+        cerr << "Untyped excepcion!!";
+        return -1;
     }
+
+
+    /*     try {                   // 异常测试
+     *         test(false);
+     *     } catch (const char* s) {  // 只能只读使用异常信息
+     *         cout << s << endl;
+     *         return -88;     // 直接结束程序
+     *     }
+     */
     return 0;
 }
+
+
+
 
 
 /*     try
@@ -27,7 +48,7 @@ int main(int argc, char* argv[])
  *         throw 42;   // 抛出异常号:数字
  *         cout << "Shouldn't ever see this" << endl;
  *     }
- * 
+ *
  *     catch (int error)  // 获得异常号的副本，输出
  *     {
  *         cout << "Error: caught exception " << error << endl;
