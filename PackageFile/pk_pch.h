95cbcc17150c363c5f06d91d54cc6b48b8d2cfff
hongwenjun <hongwenjun@gmail.com>
2012-11-25 12:12:21 +0000

    PackageFile/pk_pch.h

---
+++ PackageFile/pk_pch.h
@@ -0,0 +1,22 @@
+#ifndef PK_PCH_H_INCLUDED
+#define PK_PCH_H_INCLUDED
+
+/*
+** Copyright (C) Http://sRGB.GoogleCode.com All rights reserved.
+*/
+
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <map>
+#include <fstream>
+#include <cstdio>
+#include <cctype>
+#include <regex>
+#include <windows.h>
+
+using namespace std;
+
+
+
+#endif // PK_PCH_H_INCLUDED
