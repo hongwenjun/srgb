4defcff44c96ddbeb8f2fd0778efa08a0883057c
hongwenjun <hongwenjun@gmail.com>
2012-12-07 16:16:22 +0000

    正确的写法要这样     while (!feof(wfp)) {         fwscanf(wfp, L"%c", &wch) ;     或者 while (fwscanf(wfp, L"%c", &wch) > 0 )
        这样写不正确，文件到EOF时，VC返回是 EOF (-1)，不能结束循环
        while (fwscanf(wfp, L"%c", &wch)) {  // GCC支持，VC好像又不支持了

--- 备用代码/wchar_t.cpp
+++ 备用代码/wchar_t.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <fstream>
 #include <map>
+#include <wchar.h>
 
 using namespace std;
 
@@ -15,12 +16,23 @@ int main()
     wchar_t wch;
     map<wchar_t , int> mci; // 装载字母计数
 
-    while (fwscanf(wfp, L"%c", &wch)) {  // GCC支持，VC好像又不支持了
+    while (!feof(wfp)) {
+        fwscanf(wfp, L"%c", &wch) ;
         ++mci[wch];  // 计数器加1
     }
     for (auto it = mci.begin(); it != mci.end(); it++)
-        wcout << (*it).first << " => " << (*it).second << "\n";
+        wcout << (*it).first << " => " << (*it).second << "\t";
 }
+
+/* 正确的写法要这样
+    while (!feof(wfp)) {
+        fwscanf(wfp, L"%c", &wch) ;
+    或者 while (fwscanf(wfp, L"%c", &wch) > 0 )
+
+    这样写不正确，文件到EOF时，VC返回是 EOF (-1)，不能结束循环
+    while (fwscanf(wfp, L"%c", &wch)) {  // GCC支持，VC好像又不支持了
+
+*/
 
 /* GCC 不支持以下  C++流的宽字节输入
     wifstream inFile("gbk.txt");
