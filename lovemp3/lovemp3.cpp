c4881f381bdbe65ba4c21d0870e3e1abeb6d4d81
hongwenjun <hongwenjun@gmail.com>
2013-06-04 06:50:16 +0000

    Shine Dion -- The Well Shine Dion -- Ocean Rose

---
+++ lovemp3/lovemp3.cpp
@@ -0,0 +1,75 @@
+田园般的宁静   Shine Dion -- The Well
+http://www.mtyyw.com/qingyinyue/shine-dion-the-well/
+/*
+Shine Dion是来自挪威Skien(斯基恩)的2重唱组合，它的音乐被认为是挪威和爱尔兰民调的结合体,在音乐中经常使用很多乐器：小提琴、风笛、SAX、曼陀林(类似我们的琵琶)、3角铁、美乐特朗(电子琴)、木吉他。其中Janne Hansen是主唱兼歌词创作，Per Selör则主作曲和吉他弹奏。 歌曲的歌词灵感通常都是来自挪威当地的神话，传说及风土人情，他们的音乐成功实现了古典和现代的结合，从中我们可以感受到田园般的宁静，灵魂的脆弱，及时间的无涯。
+*/
+
+歌词：
+By an old drinking well
+On the grass so green
+古井幽幽 绿草青青
+I lay down and fell into a dream
+在水之湄，傍井而眠
+By a strange mellow sound
+From a silver eyed bird
+梦里依稀 白鸟飞过
+其声如磁 对吾轻鸣
+I opened me eyes but said no word
+轻启双眸，四处寂静
+On the pathway came walking
+A woman so bold
+蓦然回首 村妇而过
+Behind her in silence went the old
+背影萧瑟 沧沧桑桑
+Ahead ran the children
+四野之中
+With joyful cries
+孩提烂漫
+Their faces were lifted to the skies
+天空之中
+尽情回响
+The woman’s hair was raven
+发如墨
+And black as the night
+墨如夜
+Covered with stars shining bright
+星光暗淡
+As they passed by the well
+漫漫而去
+I saw that their eyes
+Were flaming like many coloured ice
+眼神冰冷 热情而结
+They passed by the well
+漫漫而过
+Like a breeze pure and clean
+清风浮过
+Soon they were nowhere to be seen
+其影隐没
+When I woke up alone
+独自醒来
+On the grass so green
+芳草青青
+I looked into the well to catch the dream
+倚井而视 梦影依稀
+The water was rising
+清水微升
+And I felt a chill
+凉意袭来
+I willed the water to be still
+水光如镜
+There was a face in the mirror
+与己而视
+And the face out of time
+韶华已逝
+The eyes held a shimmering shine
+眼眸清亮
+There was a face in the mirror
+与己而视
+Like a face wasn’t mine
+不曾相识
+The eyes held a shimmering shine
+蓦然回首
+脉脉离去
+
+
+Shine Dion -- Ocean Rose
