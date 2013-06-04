7a78acb946c5048e48b6b31e3c527848ae3ae351
hongwenjun <hongwenjun@gmail.com>
2013-06-04 06:54:01 +0000

    Shine Dion -- Ocean Rose

--- lovemp3/lovemp3.cpp
+++ lovemp3/lovemp3.cpp
@@ -1,5 +1,8 @@
+
 田园般的宁静   Shine Dion -- The Well
-http://www.mtyyw.com/qingyinyue/shine-dion-the-well/
+
+在线试听  http://www.mtyyw.com/qingyinyue/shine-dion-the-well/
+
 /*
 Shine Dion是来自挪威Skien(斯基恩)的2重唱组合，它的音乐被认为是挪威和爱尔兰民调的结合体,在音乐中经常使用很多乐器：小提琴、风笛、SAX、曼陀林(类似我们的琵琶)、3角铁、美乐特朗(电子琴)、木吉他。其中Janne Hansen是主唱兼歌词创作，Per Selör则主作曲和吉他弹奏。 歌曲的歌词灵感通常都是来自挪威当地的神话，传说及风土人情，他们的音乐成功实现了古典和现代的结合，从中我们可以感受到田园般的宁静，灵魂的脆弱，及时间的无涯。
 */
@@ -73,3 +76,68 @@ The eyes held a shimmering shine
 
 
 Shine Dion -- Ocean Rose
+
+/*
+Shine Dion，还算熟悉的一个名字。以前推荐过《田园般的宁静Shine Dion-The well》。感觉Shine Dion的音乐，凯尔特、爱尔兰味特浓，很有田园风情，当然还有爱尔兰民谣特有的伤感气质。Shine Dion是来自挪威第七大城市Skien(斯基恩)的二重唱组合，他们的音乐被认为是挪威和爱尔兰民谣的结合体，在音乐中经常使用很多乐器：小提琴、风笛、萨克斯、曼陀林(类似中国的琵琶)、三角铁、美乐特朗(一电子琴) 、木吉他。《Ocean Rose》的忧郁气质令人无法抗拒。一个飘忽的身影，行走在爱尔兰的高地。戴上耳机，闭上眼睛，将每一个音符和歌者的每一个呼吸都听进脑海，听进内心。反复听，直听到“梦醒了，眼睛也不要睁开……”
+*/
+
+歌词：
+She was a silent white rose
+who lived in a tall iron tower
+Barefoot she wandered in her room
+dreaming of life and a lover
+And in a night she threw a net
+into the ocean wide and deep
+And some of the things she caught in there
+made her sad and made her weep
+A fiery warmth embraced her heart
+when sunrays caught her hair
+And then she looked upon the world
+her longing got hard to bear
+She was locked in an iron tower
+with windows but no doors to be found
+Often she listened hard at night
+for a voice in the ocean’s sound
+And voices was mingling with her own
+waves were singing,flee the tower
+By the stairway that takes you down
+follow the ocean’s song my flower
+Silent the rose went down
+she was shivering in the dark and the cold
+Through in an opening in the wall
+the ocean sang as fortold
+Floating upon the warm blue sea
+by the waves to a shore she was led
+Among the trees red roses grow
+and the white rose turned to red…
+
+失明的光线 几近黯然的叹息
+低语呢喃里 安静如我　
+他年清莲 已风化的身躯 噼啪燃烧的火焰
+早已作古 且让我在你怀里死去
+支离破碎 遍地齑粉 或为脓血 或为飞灰
+游走过时光后发酵酝酿 势如野草
+在遇到你之后化为春泥 化为丰美
+请来吧 携我的手 向春天奔跑
+树摇动着手臂 离开泥土 随风而上
+请将我击碎 请攻克我多年苦心堆叠的堡垒
+请撕裂我的华美冠冕
+请让我在闭目之后
+依然呼吸你薄荷的清朗
+我知道 我属于你
+以及飘动的风 流动的空气
+扶摇直上 振翅高飞　　
+且让我在你里死去
+死去 一切 在这之前
+容我暂停 容我挥手 容我告别
+彼时的瑰丽如霞 而今徒余苍凉
+切莫哭泣 放开你 放开我 放开所有
+且看 生生流转 流年如蝶 翩跹而舞 　
+安葬我 用你的手 让我静候 休憩 　　
+如奶如蜜的迦南美地
+草肥果硕的伊甸花园
+等候在你的里面 重新复活
+怀一粒莲子的心
+且让我死去 若你愿意
+请让一枝清瘦的红玫自你手中　　
+轻轻斜依在我的墓前……
