dc6bbc3fd70fe74a4d148c84253df5ec2085a73a
hongwenjun <hongwenjun@gmail.com>
2013-06-05 15:26:43 +0000

    // coreldraw_riffinfo.h

--- CorelThumbnail/coreldraw_riffinfo.h
+++ CorelThumbnail/coreldraw_riffinfo.h
@@ -24,24 +24,33 @@
 
 #define CDRE_TYPE   FCC('CDRE')    // CorelDRAW X4  文件类型 , 开始使用 PK文件头 zip打包
 #define CDRF_TYPE   FCC('CDRF')    // CorelDRAW X5
+#define CDRF_TYPE   FCC('CDRG')    // CorelDRAW X6
 
+
+
+// 00000000h: 52 49 46 46 C4 AE 00 00 43 44 72 39 76 72 73 6E ; RIFF漠..CDr9vrsn
+// 00000010h: 02 00 00 00 84 03 44 49 53 50 2C 28 00 00 08 00 ; ...._DISP__....
+#pragma pack(2)
 typedef struct coreldraw_riff_head {
     FOURCC  riff;       // 必须为 'RIFF'
     DWORD   cb;     // CorelDRAW 数据文件大小
     DWORD   file_type;  // 一般为 'CDR?'
+/*----------------------------------------*/
+    FOURCC  vrsn;  // 'vrsn'
+    DWORD   ver_cb;
+    WORD   version; // CorelDRAW  版本，一般 2字节
 } RIFF_CDR_TYPE;
 
+#pragma pack(pop)
 typedef struct _riffchunk {
-    FOURCC fcc;
-    DWORD  cb;
-} RIFFCHUNK, * LPRIFFCHUNK , CDR_VERSION;
+    FOURCC  fcc;
+    DWORD   cb;
+} RIFFCHUNK, * LPRIFFCHUNK;
 
 // 获得 CorelDRAW X3文件 为止的版本号
 int get_cdrfile_version(const char* cdr_filename);
 
 // 获得 CorelDRAW X3文件 为止 cdr_filename  展开缩略图 bmp_filename
 bool cdr_riff_disp2bmp(const char* cdr_filename, const char* bmp_filename);
-
-
 
 #endif // CORELDRAW_RIFFINFO_H_INCLUDED
