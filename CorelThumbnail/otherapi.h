8a6d3147fdc2787d3fdeab766b3afe99307a4f6f
hongwenjun <hongwenjun@gmail.com>
2013-06-05 15:18:27 +0000

    otherapi.cpp

---
+++ CorelThumbnail/otherapi.h
@@ -0,0 +1,13 @@
+// otherapi.h
+
+#ifndef OTHERAPI_H_INCLUDED_COREL_THUMBNAIL
+#define OTHERAPI_H_INCLUDED_COREL_THUMBNAIL
+
+// 检查一个文件是否存在
+BOOL IsFileExist(LPCTSTR lpFileName);
+
+// 功能 获得当前路径
+char* GetAppDir(char* szPath);
+
+
+#endif // OTHERAPI_H_INCLUDED_COREL_THUMBNAIL
