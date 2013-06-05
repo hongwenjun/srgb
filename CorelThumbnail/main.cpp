536f29390c0db16f67902b8a6eeb11e26f95e372
hongwenjun <hongwenjun@gmail.com>
2013-06-05 15:16:37 +0000

    edit

--- CorelThumbnail/main.cpp
+++ CorelThumbnail/main.cpp
@@ -1,61 +1,39 @@
-﻿#include <iostream>
-#include "ZLibWrapLib.h"
-#include "ZLib/zip.h"
-#include "ZLib/unzip.h"
+ /*
+** Copyright (C) http://srgb.googlecode.com  All rights reserved.
+*/
+#include <iostream>
+#include "corel_thumbnail.h"
 
-BOOL ZipExtractCurrentFile(unzFile uf, LPCTSTR lpszDestFolder);
 
-
-#define CASESENSITIVITY (0)
-BOOL extract_onefile(unzFile uf, const char* filename)
+void helpinfo(const char* exefile)
 {
-    int err = UNZ_OK;
-    if (unzLocateFile(uf, filename, CASESENSITIVITY) != UNZ_OK) {
-        printf("file %s not found in the zipfile\n", filename);
-        return FALSE;
-    }
-
-    printf("file %s 能够找到 in the zipfile\n", filename);
-
-    if (!ZipExtractCurrentFile(uf, "./Test/")) {
-        return FALSE;
-    }
-
-    return TRUE;
+    printf("\nCorelDRAW 缩略图转成PNG图片和检测文档版本工具\n"
+           "(C) 版权所有 2013.06 Hongwenjun (蘭公子)\n\n"
+           "Usage: %s  <CorelDRAW文件.cdr>  [SavePngFile]\n" , exefile);
 }
 
 
-int main()
+int main(int argc, char* argv[])
 {
-    using namespace std;
-
-    bool ret = ZipCompress("main.cpp", "Test/a.zip", false);
-
-    if (ret)
-        cout << "压缩成功" << endl;
-
-      ret = ZipExtract("Test/图形1.cdr", "Test/xxx");
-
-    if (ret)
-        cout << "释放成功" << endl;
 
 
-    // 解压先使用 zipOpen64 来打开一个 ZIP 文件
+    const char* cdr_filename = "test.cdr";
+    const char* png_filename = NULL;
 
-    unzFile uf = unzOpen64("Test/图形1.cdr");
+    if (argc > 1)
+        cdr_filename = argv[1];
+    if (argc > 2)
+        png_filename = argv[2];
 
-    // 需要先使用 unzGetGlobalInfo64 来取得该文件的一些信息，来了解这个压缩包里一共包含了多少个文件，等等。
-    unz_global_info64 gi;
+    bool ret =  cdr_thumbnail_png(cdr_filename, png_filename);
 
-    if (unzGetGlobalInfo64(uf, &gi) != UNZ_OK) {
-        return FALSE;
+
+    if (!ret) {
+        helpinfo(argv[0]);
+        return 88;
+
     }
-
-    cout << gi.number_entry << " : " <<gi.size_comment << endl;
-
-    extract_onefile(uf, "metadata/thumbnails/thumbnail.bmp");
 
 
     return 0;
 }
-
