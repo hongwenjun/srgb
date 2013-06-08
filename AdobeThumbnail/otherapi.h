a99bf2859ae03382b0450b7c0c83566f854895eb
hongwenjun <hongwenjun@gmail.com>
2013-06-08 16:43:31 +0000

    AdobeThumbnail

---
+++ AdobeThumbnail/otherapi.h
@@ -0,0 +1,28 @@
+﻿// otherapi.h
+
+#ifndef OTHERAPI_H_INCLUDED_COREL_THUMBNAIL
+#define OTHERAPI_H_INCLUDED_COREL_THUMBNAIL
+#include <windows.h>
+#include <stdio.h>
+
+
+// 检查一个文件是否存在
+BOOL IsFileExist(LPCTSTR lpFileName);
+
+// 功能 获得当前路径
+char* GetAppDir(char* szPath);
+
+// 得到全路径文件的文件名
+const char* GetFileBaseName(const char* szPath);
+
+
+// 获得文件大小
+size_t get_fileSize(const char* filename);
+
+// 内存匹配函数memfind
+char* memfind(const char* buf, const char* tofind, size_t len);
+
+
+
+
+#endif // OTHERAPI_H_INCLUDED_COREL_THUMBNAIL
