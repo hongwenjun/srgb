f1a47e331a756966ffac4ede3efcd75cb0a784cd
hongwenjun <hongwenjun@gmail.com>
2013-06-08 16:43:08 +0000

    AdobeThumbnail

---
+++ AdobeThumbnail/AdobeThumbnail.h
@@ -0,0 +1,26 @@
+#ifndef ADOBETHUMBNAIL_H_INCLUDED
+#define ADOBETHUMBNAIL_H_INCLUDED
+
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <string>
+#include <regex>
+#include <atlenc.h>
+
+
+// Adobe 文件提取 缩略图
+int AdobeThumbnail(const char* adobe_filename ,const char* savejpeg_filename );
+
+
+// 获得编码后的数据长度
+#define getDecodeLength Base64EncodeGetRequiredLength
+
+// 数据使用 Base64 编码, 返回编码后的长度，如果返回0，可能目标空间不够
+int toBase64_Encode(const char* pSrc, int nLenSrc, char* pDst, int nLenDst);
+
+// Base64 数据解码, 返回原来数据的长度，如果返回0，可能目标空间不够
+int fromBase64_Decode(const char* pSrc, int nLenSrc, char* pDst, int nLenDst);
+
+
+#endif // ADOBETHUMBNAIL_H_INCLUDED
