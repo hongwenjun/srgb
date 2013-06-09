eeb07f2c7830375aa771999d9132e4ea8587f2c2
hongwenjun <hongwenjun@gmail.com>
2013-06-09 03:24:58 +0000

    EPS文件缩略图 分析

--- AdobeThumbnail/AdoeFileXML.h
+++ AdobeThumbnail/AdoeFileXML.h
@@ -1,7 +1,8 @@
 // AdoeFileXML.h
 #if(0)
 
-Indd 文件缩略图 分析
+///*****************************    Indd 文件缩略图 分析        *********************************///
+
    <xmp:Thumbnails>
     <rdf:Alt>
      <rdf:li
@@ -57,5 +58,32 @@ Arial"/>
 <?xpacket end="w"?>
 
 
+///*****************************    EPS文件缩略图 分析        *********************************///
+
+<?xpacket begin="
+" id="W5M0MpCehiHzreSzNTczkc9d"?>
+<x:xmpmeta xmlns:x="adobe:ns:meta/" x:xmptk="Adobe XMP Core 5.0-c060 61.134777, 2010/02/12-17:32:00        ">
+   <rdf:RDF xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#">
+      <rdf:Description rdf:about=""
+            xmlns:pdf="http://ns.adobe.com/pdf/1.3/">
+         <pdf:Producer>Adobe PDF library 9.90</pdf:Producer>
+      </rdf:Description>
+      <rdf:Description rdf:about=""
+            xmlns:xmp="http://ns.adobe.com/xap/1.0/"
+            xmlns:xmpGImg="http://ns.adobe.com/xap/1.0/g/img/">
+         <xmp:CreateDate>2013-06-09T10:20:45+08:00</xmp:CreateDate>
+         <xmp:ModifyDate>2013-06-09T10:20:49+08:00</xmp:ModifyDate>
+         <xmp:CreatorTool>Adobe Illustrator CS5.1</xmp:CreatorTool>
+         <xmp:MetadataDate>2013-06-09T10:20:49+08:00</xmp:MetadataDate>
+         <xmp:Thumbnails>
+            <rdf:Alt>
+               <rdf:li rdf:parseType="Resource">
+                  <xmpGImg:width>256</xmpGImg:width>
+                  <xmpGImg:height>216</xmpGImg:height>
+                  <xmpGImg:format>JPEG</xmpGImg:format>
+                  <xmpGImg:image>/9j/4AAQSk    /*  Base64 编码    */       q7FXYq//Z/9k=</xmpGImg:image>
+               </rdf:li>
+            </rdf:Alt>
+         </xmp:Thumbnails>
 
 #endif
