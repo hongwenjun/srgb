f1251ea64d006a18023507dc986f41950c4696f6
hongwenjun <hongwenjun@gmail.com>
2013-06-09 03:20:10 +0000

    AdoeFileXML.h   XML数组  文件缩略图 和字体 分析

---
+++ AdobeThumbnail/AdoeFileXML.h
@@ -0,0 +1,61 @@
+// AdoeFileXML.h
+#if(0)
+
+Indd 文件缩略图 分析
+   <xmp:Thumbnails>
+    <rdf:Alt>
+     <rdf:li
+      xmpGImg:format="JPEG"
+      xmpGImg:width="256"
+      xmpGImg:height="256"
+      xmpGImg:image="/9j/4AAQSk    /*  Base64 编码    */        q7FXYq//Z"/>
+    </rdf:Alt>
+   </xmp:Thumbnails>
+
+
+   <xmpTPg:Fonts>
+    <rdf:Bag>
+     <rdf:li
+      stFnt:fontName="SimSun"
+      stFnt:fontFamily="SimSun"
+      stFnt:fontFace="Regular"
+      stFnt:fontType="TrueType"
+      stFnt:versionString="SimSunVersion 2.11"
+      stFnt:composite="false"
+      stFnt:fontFileName="simsun.ttc"/>
+     <rdf:li
+      stFnt:fontName="SimHei"
+      stFnt:fontFamily="SimHei"
+      stFnt:fontFace="Regular"
+      stFnt:fontType="TrueType"
+      stFnt:versionString="SimHeiVersion 2.10"
+      stFnt:composite="false"
+      stFnt:fontFileName="simhei.ttf"/>
+     <rdf:li
+      stFnt:fontName="ATC-9ed14f53*Arial"
+      stFnt:fontFamily="&lt;9ED1&gt;&lt;4F53&gt;Arial"
+      stFnt:fontType="ATC"
+      stFnt:versionString="ATC-9ed14f53*Arial"
+      stFnt:composite="false"
+      stFnt:fontFileName="
+Arial"/>
+     <rdf:li
+      stFnt:fontName="FZKTJW--GB1-0"
+      stFnt:fontFamily="FZKai-Z03S"
+      stFnt:fontFace="Regular"
+      stFnt:fontType="TrueType"
+      stFnt:versionString="FZKTJW--GB1-03.00"
+      stFnt:composite="false"
+      stFnt:fontFileName="FZKTJW.TTF"/>
+    </rdf:Bag>
+   </xmpTPg:Fonts>
+  </rdf:Description>
+ </rdf:RDF>
+</x:xmpmeta>
+                                                                                                    
+                                
+<?xpacket end="w"?>
+
+
+
+#endif
