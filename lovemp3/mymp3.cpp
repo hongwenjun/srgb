a977ea06f0c7b3813ffb871b5c40531ad55cfc57
srgb <hongwenjun@gmail.com>
2015-12-31 09:11:00 +0800

    Create mymp3.cpp

---
+++ lovemp3/mymp3.cpp
@@ -0,0 +1,78 @@
+
+
+https://www.youtube.com/watch?v=0it1RtfrVX4&feature=youtu.be
+50首流行歌曲 混音新音樂 超好聽 50 Top mixes. Music.
+
+0:01- Maroon 5 feat. Wiz Khalifa - "Payphone
+0:03- The Wanted - "Glad You Came"
+0:05- Flo Rida - "Whistle"
+0:08- Maroon 5 feat. Wiz Khalifa - "Payphone
+0:10- The Wanted - "Glad You Came"
+0:12- Flo Rida - "Whistle" ~0:19
+0:20- Christina Aguilera - "Your Body"
+0:24- Christina Aguilera - "Your Body"+ Usher - "Scream" +Ellie Goulding - "Lights"(0:27)
+0:35- Ellie Goulding - "Lights"
+0:39- Jennifer Lopez feat. Pitbull - "Dance Again"
+0:42- Jennifer Lopez feat. Pitbull - "Dance Again" +Brown - "Don't Wake Me Up
+0:47- LMFAO - "Sorry For Party Rocking"
+0:50- Fun- "Some nights"
+0:57- Kelly Clarkson- "Stronger"
+1:04- Maroon 5 - "Payphone"
+1:05- Far East Movement feat. Justin Bieber - "Live My Life"
+1:11- Jessie J - "Domino"
+1:12- Jessie J - "Domino" + Calvin Harris feat. Ne-Yo - "Let's Go"
+1:13- Calvin Harris feat. Ne-Yo - "Let's Go" + Chris Brown - "Turn Up The Music"
+1:28- Calvin Harris feat. Ne-Yo - "Let's Go"
+1:32- Calvin Harris feat. Ne-Yo - "Let's Go" + Maroon 5 - "One More Night"
+1:42- Jessie J - "Domino"
+1:44- Carly Rae Jepson - "Call Me Maybe"
+1:51- P!nk - "Blow Me (One Last Kiss)"
+1:58- Flo Rida feat. Sia - "Wild Ones"
+2:02- Calvin Harris feat. Ne-Yo - "Let's Go
+2:03- Calvin Harris feat. Ne-Yo - "Let's Go''+ Rita Ora - "How We Do (Party)"
+2:07- Jessie J - "Domino"
+2:13- "Let's Go''
+2:13- Jessie J - "Domino" (+ "We are young")
+2:29- Adele - "Set Fire To The Rain" (instr. We are young+ Brokenhearted)
+2:44- Katy Perry - "Part Of Me"
+3:00- Ne-Yo - "Let Me Love You "
+3:02- Demi Lovato - "Give Your Heart A Break"
+3:10- Katy Perry - "Part Of Me"
+3:16- Ne-Yo - "Let Me Love You "
+3:18- Demi Lovato - "Give Your Heart A Break"
+3:26- Katy Perry - "Part Of Me"
+3:33- Fun feat. Janelle Monáe - "We Are Young"
+3:42- "We Are Young"+Katy Perry - "Wide Awake" (3:44)+ Nelly Furtado- "Big Hoops (Bigger The Better)" (3:56-4:00)
+4:01- Justin Bieber - "Boyfriend" + Cher Lloyd - "Want U Back" (4:03)
+4:22- Owl City & Carly Rae Jepson - "Good Time"
+4:29- Nicky Minaj - "Starships"
+4:33- "Good Time"
+4:35- PSY- Gangnam Style!
+4:36- Alex Clare - "Too Close"
+4:40- Usher - "Climax"
+4:43- Justin Bieber feat. Big Sean - "As Long As You Love Me"
+4:44- Gangnam Style!
+4:48-"Good Time"
+4:51- Ke$ha - "Die Young"
+4:55- One Direction - "What Makes You Beautiful"
+4:58- "As Long As You Love Me"
+4:59- PSY- Gangnam Style
+5:03- "Good Time"
+5:07- Rihanna - "Diamonds" + PSY- Gangnam Style
+5:13- Nicky Minaj - "Pound The Alarm"
+5:14- Rihanna - "Where Have You Been"
+5:29- Gangnam Style!
+5:43- Adele - "Skyfall"
+5:46- David Guetta feat. Sia - "Titanium"
+6:15- One Direction - "Live While We're Young"
+6:26- Will.I.Am feat. Eva Simons - "This is Love"
+6:28- Pitbull feat. Chris Brown - "International Love"
+6:30- David Guetta feat. Chris Brown & Lil Wayne - "I Can Only Imagine"
+6:33- Rihanna - "Where Have You Been"
+6:37- Swedish House Mafia - "Don't You Worry Child"
+6:41- Gotye feat. Kimbra - "Somebody That I Used To Know"
+6:45- Enrique Iglesias feat. Sammy Adams - "Finally Found You"
+7:00- Zedd feat. Matthew Koma - "Spectrum"
+7:12-"Somebody That I Used To Know"
+7:15- Madonna feat. M.I.A. & Nicky Minaj - "Give Me All Your Luvin'"
+7:31-Good Time (instr. "Somebody that I used to know)
