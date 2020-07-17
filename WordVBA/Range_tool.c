02c55e9707be21f4ec9c21cb8e95c6f1c32790a1
hongwenjun <hongwenjun@gmail.com>
2020-07-17 09:57:40 +0800

    Range_tool.c  输出信息 >> L1:E8  L开始行  E结束行

--- WordVBA/Range_tool.c
+++ WordVBA/Range_tool.c
@@ -4,30 +4,36 @@
 
 int main(int argc, char* argv[])
 {
-    const char * filename = argv[1];
-    if (argc == 1)
-        printf("\nUsage: %s  filename.txt \n", argv[0] );
+    const char* filename = argv[1];
+    if (argc == 1) {
+        printf("\nUsage: %s  filename.txt \n\n  输出信息 >> L1:E8  L开始行  E结束行\n", argv[0]);
+        return -1;
+    }
 
     FILE* file = fopen(filename, "r");
 
     int cnt = 1;
     char key[64] = "NULL";
     char line[1024];
+    char* enter;
 
     while (fgets(line, 1024, file) != NULL) {
+        enter = strchr(line, '\n');
+        if (enter)
+            *enter = '\0';
 
         if (strcmp(line, key) == 0) {
             //  printf("%d\t%s", cnt, line);
         } else {
 
             strcpy(key, line);
-            printf("E%d  %sL%d:", cnt - 1,   key, cnt);
+            printf("E%d\n%s    L%d:", cnt - 1,   key, cnt);
             // L开始单元格 E 结束
         }
 
         cnt++;
     }
-
+    printf("E%d\n", cnt - 1);
     fclose(file);
     return 0;
 }
