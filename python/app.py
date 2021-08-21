c4b41e590c248d337b91c11a54f49d3b967169b6
蘭雅sRGB <hongwenjun@gmail.com>
2021-08-21 09:41:54 +0800

    Update app.py

--- python/app.py
+++ python/app.py
@@ -1,39 +1,33 @@
-import  ipdb, ipaddress
-from flask import Flask
-from flask import request, jsonify
-
-# export FLASK_ENV=development
-# flask run --host=0.0.0.0
+import ipdb, ipaddress
+from flask import Flask, request, jsonify
 
 db = ipdb.BaseStation("qqwry.ipdb")
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route("/ip/")
+@app.route("/ip/<ipaddr>")
+def show_ip(ipaddr=None):
+    # ip 地址为空获得浏览器客户端IP
+    if ipaddr is None:
+        ip = request.remote_addr
+        try:
+            _ip = request.headers["X-Real-IP"]
+            if _ip is not None:
+                ip = _ip
+        except Exception as e:
+            print(e)
+    else:
+        ip = ipaddr
 
-@app.route('/ip/<ipaddr>')
-def show_ip_profile(ipaddr): 
-    ip = ipaddr
+    # ip地址 从纯真IP数据库 搜索城市定位
     try:
         ipaddress.ip_address(ip).is_global
-        # print(ip, db.find(ip, "CN"))
         city = db.find(ip, "CN")
-        ipaddr =  ip + " @" + city[0] + city[1] + city[2] + "\n"
-    except:
-        pass
-    return ipaddr
-
-@app.route('/ip/')
-def show_ip(): 
-    ip = request.remote_addr  
-    try:
-        _ip = request.headers["X-Real-IP"]
-        if _ip is not None:
-            ip = _ip
-            ipaddress.ip_address(ip).is_global
-            city = db.find(ip, "CN")
-            ip =  ip + " @" + city[0] + city[1] + city[2] + "\n"
+        ipaddr = ip + " @" + city[0] + city[1] + city[2] + city[3] + "\n"
     except Exception as e:
         print(e)
-    return ip
+
+    return ipaddr
+
+# export FLASK_ENV=development   # 调试模式: 修改代码不用重启服务
+# flask run --host=0.0.0.0       # 监听所有公开的 IP
